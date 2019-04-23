411a66b163bfa2710f354140e538212eb4b0069e
alireza ghodrati <apps.ghodrati1377@gmail.com>
2019-04-23 09:39:06 +0430

    Fix Gps

--- app/src/main/java/ir/FiroozehCorp/SensorApp/Activities/MainActivity.kt
+++ app/src/main/java/ir/FiroozehCorp/SensorApp/Activities/MainActivity.kt
@@ -1,45 +1,210 @@
 package ir.FiroozehCorp.SensorApp.Activities
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 import ir.FiroozehCorp.SensorApp.R
 import ir.FiroozehCorp.SensorApp.Utils.SensorUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+open class MainActivity : AppCompatActivity()
+    , GoogleApiClient.ConnectionCallbacks
+    , GoogleApiClient.OnConnectionFailedListener
+    , com.google.android.gms.location.LocationListener, SensorEventListener {
 
+    private var event: SensorEvent? = null
+    private var isActive = false
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        log?.text = Gson().toJson(event)
+        this@MainActivity.event = event
     }
 
+    private val TAG = "MainActivity"
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    private var mLocationManager: LocationManager? = null
+    lateinit var mLocation: Location
+    private var mLocationRequest: LocationRequest? = null
+    private val listener: com.google.android.gms.location.LocationListener? = null
+    private val UPDATE_INTERVAL = (2 * 1000).toLong()  /* 10 secs */
+    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
+
+    lateinit var locationManager: LocationManager
+
+
+    override fun onStart() {
+        super.onStart()
+        mGoogleApiClient.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mGoogleApiClient.isConnected) {
+            mGoogleApiClient.disconnect()
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+        Log.i(TAG, "Connection Suspended")
+        mGoogleApiClient.connect()
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.errorCode)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onLocationChanged(location: Location) {
+
+        mLocation = location
+        updateLog()
+
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onConnected(p0: Bundle?) {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+
+        startLocationUpdates();
+
+        val fusedLocationProviderClient:
+                FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener(this) { location ->
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    mLocation = location
+                    updateLog()
+                }
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+
+        SensorUtil.getGyroscopeData(this, this)
+
+        mLocationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        checkLocation()
+
+
         start?.setOnClickListener {
             if (ip?.text.toString().isNotEmpty() && timer?.text.toString().isNotEmpty()) {
+                isActive = true
                 Timer().scheduleAtFixedRate(object : TimerTask() {
                     override fun run() {
-                        SensorUtil.getGyroscopeData(
-                            this@MainActivity
-                            , this@MainActivity
-                        )
+                        updateLog()
                     }
                 }, 0L, timer?.text.toString().toLong())
             } else
                 Toast.makeText(this, "Fill All Fields", Toast.LENGTH_SHORT).show()
         }
+
+
+    }
+
+    private fun checkLocation(): Boolean {
+        if (!isLocationEnabled())
+            showAlert()
+        return isLocationEnabled()
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun showAlert() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Enable Location")
+            .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " + "use this app")
+            .setPositiveButton("Location Settings") { _, _ ->
+                val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(myIntent)
+            }
+            .setNegativeButton("Cancel") { _, _ -> }
+        dialog.show()
+    }
+
+    private fun startLocationUpdates() {
+
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(UPDATE_INTERVAL)
+            .setFastestInterval(FASTEST_INTERVAL)
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+            mGoogleApiClient,
+            mLocationRequest, this
+        )
+    }
+
+    private fun updateLog() {
+        if (isActive) {
+            runOnUiThread {
+                locationLog?.text = Gson().toJson(mLocation)
+                gyroLog?.text = Gson().toJson(event)
+            }
+        }
+
     }
 }
