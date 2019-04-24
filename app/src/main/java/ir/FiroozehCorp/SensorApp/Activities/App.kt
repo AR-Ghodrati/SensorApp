8b17de82244a7bcd2488ee4baa254c5cacf60424
alirezaghodrati <apps.ghodrati1377@gmail.com>
2019-04-24 12:56:24 +0430

    Add FireBase To Project Fix Icon

--- app/src/main/java/ir/FiroozehCorp/SensorApp/Activities/App.kt
+++ app/src/main/java/ir/FiroozehCorp/SensorApp/Activities/App.kt
@@ -3,11 +3,17 @@ package ir.FiroozehCorp.SensorApp.Activities
 import android.app.Application
 import android.content.Context
 import android.support.multidex.MultiDex
+import com.google.firebase.FirebaseApp
 
 class App : Application() {
 
     override fun attachBaseContext(base: Context) {
         MultiDex.install(base)
         super.attachBaseContext(base)
+    }
+
+    override fun onCreate() {
+        FirebaseApp.initializeApp(this)
+        super.onCreate()
     }
 }
